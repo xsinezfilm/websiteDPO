1b012141da0ec35a749ef77de0c7c45c962dab95
panatthawut <137262795+panatthawut@users.noreply.github.com>
2024-04-05 09:12:09 +0700

    give to flim

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,50 @@ loginBtn.addEventListener('click', () =>{
 loginClose.addEventListener('click', () =>{
    login.classList.remove('show-login')
 })
+
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+        myslide[i].classList.remove('slideRight'); // เอาออกให้หมดก่อน
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+    myslide[counter - 1].classList.add('slideRight'); // เพิ่ม slideRight class เมื่อแสดงภาพ
+
+	dot[counter - 1].className += " active";
+}
